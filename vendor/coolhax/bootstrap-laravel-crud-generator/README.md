c381e6a5d2d504edcff24c604047b0c5ca51bdb7
nebula1991 <dbellanco3@gmail.com>
2025-01-26 23:26:03 +0100

    four commit

---
+++ vendor/coolhax/bootstrap-laravel-crud-generator/README.md
@@ -0,0 +1,104 @@
+![Laravel Crud Generator](https://banners.beyondco.de/Laravel%20CRUD.png?theme=dark&packageManager=composer+require&packageName=coolhax%2Fbootstrap-laravel-crud-generator&pattern=architect&style=style_1&description=Laravel+CRUD+Generator&md=1&showWatermark=0&fontSize=100px&images=gift)
+)
+
+
+![Packagist](https://img.shields.io/badge/Packagist-v1.3.2-green.svg?style=flat-square)
+![Licence](https://img.shields.io/badge/Licence-MIT-green.svg?style=flat-square)
+![StyleCI](https://img.shields.io/badge/StyleCI-pass-green.svg?style=flat-square)
+
+
+This Laravel Generator package provides and generate Controller, Model (with eloquent relations) and Views in **Bootstrap** for your development of your applications with single command.
+
+This packege based on - (https://github.com/awais-vteams/laravel-crud-generator)
+
+**What Is New**
+- **Relationship** with select in form and auto Relationship data pass in view 
+- **Select dropDown in form**
+- **Search Filter added**
+- **$serchable Property in Model**
+- Bootstrap new Design **col-md-6**
+- New Feature **Auto Route**
+
+
+
+- Will create **Model** with Eloquent relations
+- Will create **Controller** with all resources
+- Will create **views** in Bootstrap
+
+## Requirements
+    Laravel >= 9.0
+    PHP >= 8.1
+    Bootstrap >= 5.0.0
+
+## Start Up
+```
+composer create-project laravel/laravel project-name
+```
+```
+cd project-name
+```
+```
+composer require laravel/ui
+```
+```
+php artisan ui bootstrap --auth
+```
+```
+npm install
+```
+```
+npm run build
+```
+## Installation
+1 - Install
+```
+composer require coolhax/bootstrap-laravel-crud-generator --dev
+```
+2- Publish the default package's config
+```
+php artisan vendor:publish --tag=crud
+```
+
+## Usage
+```
+php artisan make:crud {table_name}
+
+php artisan make:crud banks
+```
+
+Add a route in `web.php`
+
+- Now auto Route Feature **Not Needed**
+```
+Route::resource('banks', App\Http\Controlles\BankController:class);
+```
+Route name in plural slug case.
+
+#### Options
+ - Custom Route
+```
+php artisan make:crud {table_name} --route={route_name}
+```
+
+## Example
+
+*Model*
+![Model](https://i.imgur.com/zTSoYvJ.png)
+
+
+*Controller*
+![Controller](https://i.imgur.com/G1ytmcL.png)
+
+
+*Listing*
+![Listing](https://i.imgur.com/UH5XGuw.png)
+
+
+*Form*
+![Form](https://i.imgur.com/poRiZRO.png)
+
+
+## Author
+
+Rohit Kumar // [Email Me](mailto:coolelearning.php@gmail.com)
+
---
+++ vendor/coolhax/bootstrap-laravel-crud-generator/src/Commands/CoolhaxCrudGenerator.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Coolhax\CoolhaxCrudGenerator\Commands;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+/**
+ * Class CoolhaxCrudGenerator.
+ *
+ * @author  Awais <asargodha@gmail.com>
+ */
+class CoolhaxCrudGenerator extends CoolhaxGeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:crud
+                            {name : Table name}
+                            {--route= : Custom route name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create bootstrap CRUD operations';
+
+    /**
+     * Execute the console command.
+     *
+     * @return bool|null
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     *
+     */
+    public function handle()
+    {
+        $this->info('Running Crud Generator ...');
+
+        $this->table = $this->getNameInput();
+
+        // If table not exist in DB return
+        if (!$this->tableExists()) {
+            $this->error("`{$this->table}` table not exist");
+
+            return false;
+        }
+
+        // Build the class name from table name
+        $this->name = $this->_buildClassName();
+
+        // Generate the crud
+        $this->buildOptions()
+            ->buildController()
+            ->buildModel()
+            ->buildViews();
+
+        $this->info('Created Successfully.');
+        $this->generateRoutes();
+        return true;
+    }
+
+    /**
+     * Build the Controller Class and save in app/Http/Controllers.
+     *
+     * @return $this
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     *
+     */
+    protected function buildController()
+    {
+        $controllerPath = $this->_getControllerPath($this->name);
+
+        if ($this->files->exists($controllerPath) && $this->ask('Already exist Controller. Do you want overwrite (y/n)?', 'y') == 'n') {
+            return $this;
+        }
+
+        $this->info('Creating Controller ...');
+
+        $replace = $this->buildReplacements();
+
+        $controllerTemplate = str_replace(
+            array_keys($replace), array_values($replace), $this->getStub('Controller')
+        );
+
+        $this->write($controllerPath, $controllerTemplate);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     *
+     */
+    protected function buildModel()
+    {
+        $modelPath = $this->_getModelPath($this->name);
+
+        if ($this->files->exists($modelPath) && $this->ask('Already exist Model. Do you want overwrite (y/n)?', 'y') == 'n') {
+            return $this;
+        }
+
+        $this->info('Creating Model ...');
+
+        // Make the models attributes and replacement
+        $replace = array_merge($this->buildReplacements(), $this->modelReplacements());
+
+        $modelTemplate = str_replace(
+            array_keys($replace), array_values($replace), $this->getStub('Model')
+        );
+
+        $this->write($modelPath, $modelTemplate);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     *
+     * @throws \Exception
+     */
+    protected function buildViews()
+    {
+        $this->info('Creating Views ...');
+
+        $tableHead = "\n";
+        $tableBody = "\n";
+        $viewRows = "\n";
+        $form = "\n";
+
+        foreach ($this->getFilteredColumns() as $column) {
+            $title = Str::title(str_replace('_', ' ', str_replace('_id', ' ', $column)));
+            $tableHead .= $this->getHead($title);
+            $tableBody .= $this->getBody($column);
+            $viewRows .= $this->getField($title, $column, 'view-field');
+            $form .= $this->getField($title, $column, 'form-field');
+        }
+
+        $replace = array_merge($this->buildReplacements(), [
+            '{{tableHeader}}' => $tableHead,
+            '{{tableBody}}' => $tableBody,
+            '{{viewRows}}' => $viewRows,
+            '{{form}}' => $form,
+        ]);
+
+        $this->buildLayout();
+
+        foreach (['index', 'create', 'edit', 'form', 'show'] as $view) {
+            $viewTemplate = str_replace(
+                array_keys($replace), array_values($replace), $this->getStub("views/{$view}")
+            );
+
+            $this->write($this->_getViewPath($view), $viewTemplate);
+        }
+
+        return $this;
+    }
+
+    protected function generateRoutes()
+    {
+        $route =$this->options['route'] ?? Str::kebab(Str::plural($this->name));
+        $routePath = base_path('routes/web.php');
+        $routeContent = File::get($routePath);
+
+        // Define the route string to check and add
+        $routeString = "Route::resource('" . strtolower($route) . "', App\Http\Controllers\\" . $this->name . "Controller::class);";
+
+        // Check if the route string already exists in web.php
+        if (strpos($routeContent, $routeString) === false) {
+            // If not, append the route string to the file
+            File::append($routePath, "\n" . $routeString);
+            $this->info('Route added successfully.');
+        } else {
+            $this->info('Route already exists.');
+        }
+    }
+
+    /**
+     * Make the class name from table name.
+     *
+     * @return string
+     */
+    private function _buildClassName()
+    {
+        return Str::studly(Str::singular($this->table));
+    }
+}
